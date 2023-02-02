37403839ec2d5dce06e97620dae48851fbd1750a
WhitneySt <whitneystena418@gmail.com>
2023-02-02 13:19:21 -0500

    create new repository

---
+++ src/app/scripts/UI.js
@@ -0,0 +1,126 @@
+//Imports
+import logo from "../../assets/images/logo.png";
+import search from "../../assets/images/search.png";
+import star from "../../assets/images/star.png";
+import { getMovieById } from "./services";
+
+//Generals
+const URL_MEDIA = "https://image.tmdb.org/t/p/original/";
+
+//DOM elements
+const logoImage = document.getElementById("logoImage");
+const searchImage = document.getElementById("searchImage");
+const containerMovies = document.getElementById("container__movies");
+export const buttonNext = document.getElementById("paginator__next");
+export const buttonPrevius = document.getElementById("paginator__previus");
+export const buttonSearch = document.getElementById("buttonSearch");
+export const inputSearch = document.getElementById("inputSearch");
+export const menuAll = document.getElementById("menuAll");
+export const menuRated = document.getElementById("menuRated");
+export const menuRecent = document.getElementById("menuRecent");
+export const menuFavorites = document.getElementById("menuFavorites");
+export const navHeader = document.getElementById("navHeader");
+const modalContainer = document.getElementById("modal__container");
+
+const modal = document.getElementById("modal");
+
+export const renderImages = () => {
+  logoImage.src = logo;
+  searchImage.src = search;
+};
+
+export const renderMovies = (listMovies) => {
+  containerMovies.innerHTML = ``;
+  listMovies.forEach((movie) => {
+    containerMovies.innerHTML += `
+      <article class="movie" data-id=${movie.id}>
+        <div class="movie__rated" data-id=${
+          movie.id
+        }><img alt="Estrella" class="starImg" src="${star}" data-id=${movie.id}>
+        <span class="movie__rated__number" data-id=${movie.id}>${
+      movie.vote_average
+    }</span></div>
+          <figure class="movie__figure" data-id=${
+            movie.id
+          }><img src="${`${URL_MEDIA}${movie.poster_path}`}" alt="Gato con botas" class="movie__image" data-id=${
+      movie.id
+    }></figure>
+      </article>`;
+  });
+};
+
+containerMovies.addEventListener("click", async (e) => {
+  if (
+    e.target.classList.contains("movie") ||
+    e.target.classList.contains("movie__rated") ||
+    e.target.classList.contains("starImg") ||
+    e.target.classList.contains("movie__figure") ||
+    e.target.classList.contains("movie__image") ||
+    e.target.classList.contains("movie__rated__number")
+  ) {
+    modalContainer.classList.remove("hidden");
+    const idMovie = e.target.getAttribute("data-id");
+    console.log(idMovie);
+    const response = await getMovieById(idMovie);
+    console.log(response);
+    renderSinapsis(response);
+    //render data into modal
+  }
+});
+
+//Event over modal
+modalContainer.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target.id === "modal__container") {
+    modalContainer.classList.add("hidden");
+  }
+  if (e.target.classList.contains("modal__button")) {
+    const favorites = sessionStorage.getItem('favorite') ? JSON.parse(sessionStorage.getItem('favorite')) : [];
+    const idMovie = e.target.getAttribute("data-id");
+    if (!favorites.includes(idMovie)) {
+      favorites.push(idMovie);
+      console.log(favorites);
+      sessionStorage.setItem('favorite', JSON.stringify(favorites));
+    }
+  }
+});
+
+//Función para mostrar los detalles de la película
+const renderSinapsis = (movieInfo) => {
+  modal.innerHTML = `
+        <article class="movie" data-id=${movieInfo.id}>
+        <div class="movie__rated" data-id=${
+          movieInfo.id
+        }><img alt="Estrella" class="starImg" src="${star}" data-id=${
+    movieInfo.id
+  }>
+        <span class="movie__rated__number" data-id=${movieInfo.id}>${
+    movieInfo.vote_average
+  }</span></div>
+          <figure class="movie__figure" data-id=${
+            movieInfo.id
+          }><img src="${`${URL_MEDIA}${movieInfo.poster_path}`}" alt="Gato con botas" class="movie__image" data-id=${
+    movieInfo.id
+  }></figure>
+      </article>
+      <section class="modal__sinapsis">
+          <h2 class="modal__title">${movieInfo.title}</h2>
+          <p class="modal__description">${movieInfo.overview}</p>
+          <div><span>${movieInfo.release_date}</span><span>${renderGenres(
+    movieInfo.genres
+  )}</span><span>${movieInfo.runtime} min</span></div>
+          <button class="modal__button" data-id=${
+            movieInfo.id
+          }>Enviar a Favoritos</button>
+        </section>
+  `;
+};
+
+const renderGenres = (arrayGenres) => {
+  let genreStrg = '';
+  arrayGenres.forEach(gener => {
+    genreStrg += `${gener.name} `;
+ 
+  })
+  return genreStrg;
+}
---
+++ src/app/scripts/main.js
@@ -0,0 +1,141 @@
+import "../styles/style.scss";
+import {
+  getAllMovies,
+  getMoreRated,
+  getRecents,
+  searchMovie,
+} from "./services";
+import {
+  buttonNext,
+  buttonPrevius,
+  buttonSearch,
+  inputSearch,
+  menuAll,
+  menuFavorites,
+  menuRated,
+  menuRecent,
+  renderImages,
+  renderMovies,
+} from "./UI";
+
+let page = 1;
+let status = "all";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const movies = await getAllMovies(page);
+  renderImages();
+  renderMovies(movies.results);
+  disabledButtons();
+});
+
+const changeStatus = async () => {
+  disabledButtons();
+  const text = inputSearch.value;
+  let movies;
+  switch (status) {
+    case "all":
+      movies = await getAllMovies(page);
+      break;
+
+    case "search":
+      movies = await searchMovie(text, page);
+      break;
+
+    case "rated":
+      movies = await getMoreRated(page);
+      break;
+
+    case "recent":
+      movies = await getRecents(page);
+      break;
+
+    // case "favorites":
+    //   // movies = await getRecents(page);
+    //   break;
+    
+    default:
+      movies = await getAllMovies(page);
+      break;
+  }
+  renderMovies(movies.results);
+}
+
+buttonNext.addEventListener("click", async () => {
+  if (page < 500) {
+    page++;
+  }
+  changeStatus();
+});
+
+buttonPrevius.addEventListener("click", async () => {
+  if (page > 1) {
+    page--;
+  }
+  changeStatus();
+});
+
+const disabledButtons = () => {
+  if (page === 1) {
+    buttonPrevius.classList.add("paginator__disabled");
+  } else if (page > 1) {
+    buttonPrevius.classList.remove("paginator__disabled");
+  }
+
+  if (page === 500) {
+    buttonNext.classList.add("paginator__disabled");
+  } else if (page < 500) {
+    buttonNext.classList.remove("paginator__disabled");
+  }
+};
+
+const deleteClassActive = (idElement) => {
+  Array.from(navHeader.children).forEach(element => {
+    if (idElement !== element.id) {
+      element.classList.remove('menu__item-active');
+    }else {
+      element.classList.add('menu__item-active')
+    }
+  });
+}
+
+buttonSearch.addEventListener("click", async () => {
+  page = 1;
+  const text = inputSearch.value;
+  const data = await searchMovie(text, page);
+  renderMovies(data.results);
+  status = "search";
+});
+
+menuAll.addEventListener("click", async () => {
+  deleteClassActive('menuAll');
+  status = "all";
+  page = 1;
+  const data = await getAllMovies(page);
+  renderMovies(data.results);
+});
+
+menuRated.addEventListener("click", async () => {
+  deleteClassActive('menuRated');
+  status = "rated";
+  page = 1;
+  const data = await getMoreRated(page);
+  renderMovies(data.results);
+});
+
+menuRecent.addEventListener("click", async () => {
+  deleteClassActive('menuRecent');
+  status = "recent";
+  page = 1;
+  const data = await getRecents(page);
+  renderMovies(data.results);
+});
+
+menuFavorites.addEventListener("click", async () => {
+  deleteClassActive("menuFavorites");
+  status = "favorites";
+  console.log('Ir a fav');
+  // page = 1;
+  // const data = await getRecents(page);
+  // renderMovies(data.results);
+});
+
